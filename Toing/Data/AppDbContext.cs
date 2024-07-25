3f19aca0ef189d0c4d694b185a18d9feee724bed
root <root@DESKTOP-ARBBD6J>
2024-07-26 06:51:38 +0900

    add db models proto

---
+++ Toing/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Toing.Data
+{
+    public class AddDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
--- Toing/Program.cs
+++ Toing/Program.cs
@@ -15,6 +15,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 var summaries = new[]
 {
